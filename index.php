c2ea731a10c32a76d8d7d8f12ba62e1a8536107c
Ellen Gomes Silva <ellen.g@academico.ifrn.edu.br>
2022-01-04 14:09:37 -0300

    Configuração do banco de dados

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Oi</title>
 </head>
 <body>
-    
+    <h1>App da gatahhh</h1>
+    <?php
+    require_once "model/Conexao.php";
+    $tecnologias = Conexao::select("tecnologia", "id, nome");
+    print_r($tecnologias);
+    ?>
 </body>
 </html>
